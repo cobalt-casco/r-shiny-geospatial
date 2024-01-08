a966ea1b3f37618e96c3f5f495514973bf302228
jebyrnes <jarrett.byrnes@umb.edu>
2024-01-08 16:58:14 -0500

    update initial commit

--- episodes/setup.R
+++ episodes/setup.R
@@ -5,11 +5,3 @@ if(!webshot::is_phantomjs_installed()){
   webshot::install_phantomjs()
 }
 
-
-
-if (! file.exists("data/maine_gov_seagrass")) {
-  dest <- tempfile()
-  download.file("https://figshare.com/ndownloader/files/43930989", dest,
-                mode = "wb")
-  unzip(dest, exdir = "data")
-}
